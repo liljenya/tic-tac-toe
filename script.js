d31896bb377792f4713c55751fd0a546dd0d4457
liljenya <kristapen13@gmail.com>
2025-05-14 14:52:39 +0300

    At last tic-tac-toe is completed

--- script.js
+++ script.js
@@ -1,17 +1,50 @@
 const startButton = document.querySelector('.start-button');
 const centeredContent = document.querySelector('.centered-content');
 
+const playerX = Player('X');
+const playerO = Player('0');
+let current = playerX;
+let gameboard = Gameboard();
+
 startButton.addEventListener('click', function () {
     centeredContent.innerHTML = '';
 
     const grid = document.createElement('div');
     grid.className = 'square';
 
-    for (let i = 1; i <= 9; i++) {
+    for (let i = 0; i < 9; i++) {
         const cell = document.createElement('div');
         cell.className = 'cell';
         grid.appendChild(cell);
+
+        cell.addEventListener('click', function () {
+            if (makeMove(gameboard, i, current.move)) {
+                cell.textContent = current.move;
+                if (checkWinner(gameboard.moves, current.move)) {
+                    window.alert(`PLayer ${current.move} has won`);
+                }
+                if (!gameboard.moves.includes('')) {
+                    return alert('Its a draw!');
+                }
+                switchPlayer();
+            }
+        })
     }
+    const resetBtn = document.createElement('button');
+    resetBtn.className = 'reset-btn';
+    resetBtn.textContent = 'Reset';
+
+    centeredContent.appendChild(resetBtn);
+
+    resetBtn.addEventListener('click', function () {
+        gameboard = new Gameboard();
+        current = playerO;       
+
+        centeredContent.innerHTML = ''; 
+
+        startButton.click(); 
+    })
+
 
     centeredContent.appendChild(grid);
 });
@@ -29,20 +62,16 @@ function Player(move) {
     };
 }
 
-const playerX = Player('X');
-const playerO = Player('0');
-let current = playerX;
-const gameboard = Gameboard();
-
-function ControlFlow(gameboard, position, symbol) {
+function makeMove(gameboard, position, symbol) {
     //todo: check if available
-    if (gameboard.moves[position] === null) {
+    const madeMove = gameboard.moves[position] === '';
+    if (madeMove) {
         gameboard.moves[position] = symbol;
-        gameboard.push(symbol);
     }
     else {
         window.alert('Stop!This place is not empty!');
     }
+    return madeMove;
 }
 
 function checkWinner(moves, symbol) {
@@ -58,15 +87,13 @@ function checkWinner(moves, symbol) {
         [2, 4, 6]
     ]
 
-    for (let combo in winCombo) {
+    for (let combo of winCombo) {
         const [a, b, c] = combo;
         if (moves[a] === symbol && moves[b] === symbol && moves[c] === symbol) {
             return true;
         }
-        else {
-            return false;
-        }
     }
+    return false;
 }
 
 function switchPlayer() {
